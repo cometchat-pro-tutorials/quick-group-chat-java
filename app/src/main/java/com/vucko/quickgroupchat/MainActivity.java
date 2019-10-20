6c8d2df096f0cd1b3b65fedd035b5e2b4b7424c0
Vucko <vucic94@yahoo.com>
2019-10-20 10:14:36 +0200

    CometChat initialization

--- app/src/main/java/com/vucko/quickgroupchat/MainActivity.java
+++ app/src/main/java/com/vucko/quickgroupchat/MainActivity.java
@@ -3,12 +3,34 @@ package com.vucko.quickgroupchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.cometchat.pro.core.CometChat;
+import com.cometchat.pro.exceptions.CometChatException;
+import com.vucko.quickgroupchat.util.Constants;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initCometChat();
+    }
+
+    private void initCometChat(){
+        CometChat.init(this, Constants.COMET_CHAT_APP_ID, new CometChat.CallbackListener<String>() {
+            @Override
+            public void onSuccess(String successMessage) {
+                Log.d(TAG, "Initialization completed successfully");
+            }
+            @Override
+            public void onError(CometChatException e) {
+                Log.d(TAG, "Initialization failed with exception: " + e.getMessage());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/vucko/quickgroupchat/util/Constants.java
@@ -0,0 +1,6 @@
+package com.vucko.quickgroupchat.util;
+
+public class Constants {
+    public static String COMET_CHAT_API_KEY = "1dbdd4cdac65f3d825070124067bfb4f3b52921e";
+    public static String COMET_CHAT_APP_ID = "9975400c9b2d5f";
+}
